62b2b242106ca4399dc624d0c990ff9a1b9d6907
amanscisingh <aman.singh.civ19@itbhu.ac.in>
2021-10-05 12:16:15 +0530

    server running well

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config({ path: './config/.env' })
+
+const app = express();
+
+const URL = process.env.MONGODB_URI.toString();
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send('Everything seems fine! You just landed the homepage of UBA Data API!!!');
+})
+
+mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+}).catch(err => {console.error(err)});
